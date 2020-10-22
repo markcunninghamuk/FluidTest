3126d871e9dbe32bd429aee33bc9abc6bc898bb1
Aidan O'Leary <aidanoleary92@gmail.com>
2020-10-22 16:51:50 +0100

    Added WaitFor method to the record service.

---
+++ Engine/Interfaces/IWaitableAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marktek.Fluent.Testing.Engine.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IWaitableAction
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void Execute();
+    }
+}
