828aa12dd0f6736552ecd7830640e448d2b51430
Mark Cunningham <mark_cunningham@outlook.com>
2021-09-10 09:29:08 +0100

    Feature/cosmos (#23)
    Added CosmosDB

---
+++ Extensions/FluidTest.CosmosDB/Validators/ContainerMustNotBeNull.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Cosmos;
+
+namespace FluidTest.CosmosDB.Validators
+{
+    internal class ContainerMustNotBeNull : ISpecificationValidator<Container>
+    {
+        public void Validate(Container item)
+        {
+            item.Should().NotBeNull();
+        }
+    }
+}
