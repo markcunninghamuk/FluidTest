beafbc3fd94821de86f40657d477f54ead484263
Mark Cunningham <mark_cunningham@outlook.com>
2021-09-10 09:10:41 +0100

    Cosmos support and path filtering

---
+++ Extensions/FluidTest.CosmosDB/Assertions/CosmosContainerShouldExist.cs
@@ -0,0 +1,37 @@
+﻿using FluidTest.CosmosDB.Validators;
+using Marktek.Fluent.Testing.Engine;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluidTest.CosmosDB.Assertions
+{
+    public class CosmosContainerShouldExist : BaseValidator<string, Container>
+    {
+        private CosmosClient client;
+        private string containerId;
+        private string databaseName;
+
+        public CosmosContainerShouldExist(CosmosClient c, string containerId, string databaseName)
+        {
+            this.client = c;
+            this.containerId = containerId;
+            this.databaseName = databaseName;
+        }
+
+        public override Container GetRecord(string id)
+        {
+            return client.GetContainer(this.databaseName, this.containerId);
+        }
+
+        public override List<ISpecificationValidator<Container>> GetValidators()
+        {
+            return new List<ISpecificationValidator<Container>>
+            {
+                new ContainerMustNotBeNull(),
+            };
+        }
+    }
+}
---
+++ Extensions/FluidTest.CosmosDB/Validators/ContainerMustNotBeNull.cs
@@ -0,0 +1,13 @@
+﻿using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Cosmos;
+
+namespace FluidTest.CosmosDB.Validators
+{
+    internal class ContainerMustNotBeNull : ISpecificationValidator<Container>
+    {
+        public void Validate(Container item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
