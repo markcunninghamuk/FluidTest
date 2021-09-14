80f7540eb014a2f517c203c7773bf7cb79eff6ad
Mark Cunningham <mark_cunningham@outlook.com>
2021-09-14 16:15:29 +0100

    Added cosmos bindings for schema validation

---
+++ Extensions/FluidTest.CosmosDB/Assertions/CosmosDocumentSchemaShouldBe.cs
@@ -0,0 +1,50 @@
+﻿using FluidTest.CosmosDB.Validators;
+using Marktek.Fluent.Testing.Engine;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FluidTest.CosmosDB.Assertions
+{
+
+    public class CosmosDocumentSchemaShouldBeValid : BaseValidator<string, IEnumerable<object>>
+    {
+        private CosmosClient cosmosClient;
+        private string schema;
+        private string containerName;
+        private string databaseName;
+
+        public CosmosDocumentSchemaShouldBeValid(CosmosClient client, string schemaFile, string containerName, string databaseName)
+        {
+            this.cosmosClient = client;
+            this.schema = schemaFile;
+            this.containerName = containerName;
+            this.databaseName = databaseName;
+        }
+
+        public override IEnumerable<object> GetRecord(string id)
+        {
+            var document = this.cosmosClient.GetContainer(databaseName, containerName).GetItemQueryIterator<object>("select * from c");
+
+            List<object> docs = new List<object>();
+
+            while (document.HasMoreResults)
+            {
+                var response = document.ReadNextAsync().GetAwaiter().GetResult();
+                docs.AddRange(response.Resource);
+            }
+
+            return docs;
+
+        }
+        public override List<ISpecificationValidator<IEnumerable<object>>> GetValidators()
+        {
+            return new List<ISpecificationValidator<IEnumerable<object>>>
+            {
+                new CosmosDocumentSchemaShouldMatch(this.schema)
+            };
+        }
+    }
+}
---
+++ Extensions/FluidTest.CosmosDB/Validators/CosmosDocumentSchemaShouldMatch.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace FluidTest.CosmosDB.Validators
+{
+    public class CosmosDocumentSchemaShouldMatch : ISpecificationValidator<IEnumerable<object>>
+    {
+        private string schemaFileName;
+
+        public CosmosDocumentSchemaShouldMatch(string schema)
+        {
+            this.schemaFileName = schema;
+        }
+
+        public void Validate(IEnumerable<dynamic> item)
+        {
+            var sourceSchema = $"{ Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}/TestCases/Schema/{schemaFileName}";
+
+            JSchema schema = JSchema.Parse(sourceSchema);
+
+            foreach (var document in item)
+            {
+                IList<string> messages;
+                JObject model = JObject.Parse(document.ToString());
+
+                bool valid = model.IsValid(schema, out messages);
+
+                foreach (var validationError in messages)
+                {
+                    Console.WriteLine($"Schema validation failed with error {validationError}");
+                }
+
+                valid.Should().BeTrue();
+
+            }
+        }
+    }
+}
