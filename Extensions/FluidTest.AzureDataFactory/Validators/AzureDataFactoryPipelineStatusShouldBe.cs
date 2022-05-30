3d9100dea1a9e714edcb7454c2228021fbc1dd3a
Mark Cunningham <mark_cunningham@outlook.com>
2022-05-30 10:29:20 +0100

    Added Data Factory Initial Bindings

---
+++ Extensions/FluidTest.AzureDataFactory/Validators/AzureDataFactoryPipelineStatusShouldBe.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Management.DataFactory.Models;
+
+namespace FluidTest.AzureSynapse.Validators
+{
+    public class AzureDataFactoryPipelineStatusShouldBe : ISpecificationValidator<PipelineRun>
+    {
+        private string status;
+
+        public AzureDataFactoryPipelineStatusShouldBe(string status)
+        {
+            this.status = status;
+        }
+        public void Validate(PipelineRun item)
+        {
+            item.Status.Should().Be(this.status);
+        }
+    }
+}
