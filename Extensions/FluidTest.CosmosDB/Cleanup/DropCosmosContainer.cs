5c950312492359cba52295aa71897c06461f5f4f
Mark Cunningham <mark_cunningham@outlook.com>
2022-06-22 17:22:41 +0100

    delete container

---
+++ Extensions/FluidTest.CosmosDB/Cleanup/DropCosmosContainer.cs
@@ -0,0 +1,27 @@
+﻿using Marktek.Fluent.Testing.Engine.Interfaces;
+using MarkTek.Fluent.Testing.RecordGeneration;
+using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+
+
+namespace FluidTest.CosmosDB
+{
+    public class DropCosmosContainer : IRecordCleanup<string>
+    {
+        private string databaseName;
+        private string container;
+        private CosmosClient client;
+
+        public DropCosmosContainer(string databaseName, string container, CosmosClient client)
+        {
+            this.databaseName = databaseName;
+            this.container = container;
+            this.client = client;
+        }
+
+        public void Cleanup(List<Record<object, string>> records, string aggregateId)
+        {
+            client.GetDatabase(databaseName).GetContainer(this.container).DeleteContainerAsync().Wait();
+        }
+    }
+}
