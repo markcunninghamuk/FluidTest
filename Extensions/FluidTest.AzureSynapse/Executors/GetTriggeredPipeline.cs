0f568b6bb2f6e48c845f634895ac79165cd879e1
Tom Ashworth <thomas.d.ashworth@gmail.com>
2022-12-02 10:01:36 +0000

    feat: support filtering Azure Synapse triggered run by their parameters (#52)

--- Extensions/FluidTest.AzureSynapse/Executors/GetTriggeredPipeline.cs
+++ Extensions/FluidTest.AzureSynapse/Executors/GetTriggeredPipeline.cs
@@ -13,24 +13,36 @@ namespace FluidTest.AzureSynapse.Executors
         private string pipelineName;
         private PipelineRunClient pipelineClient;
         private DateTimeOffset executionTime;
+        private IDictionary<string, string> parameterFilters;
 
-        public GetTriggeredPipeline(string pipelineName, PipelineRunClient pipelineClient, DateTimeOffset executionTime)
+        public GetTriggeredPipeline(string pipelineName, PipelineRunClient pipelineClient, DateTimeOffset executionTime, IDictionary<string, string> parameterFilters = default)
         {
             this.pipelineName = pipelineName;
             this.pipelineClient = pipelineClient;
             this.executionTime = executionTime;
+            this.parameterFilters = parameterFilters;
         }
 
         public Record<PipelineRun, string> CreateRecord()
         {
             var filter = new RunFilterParameters(executionTime, executionTime.AddHours(1));
             filter.Filters.Add(new RunQueryFilter(RunQueryFilterOperand.PipelineName, RunQueryFilterOperator.In, new List<string>() { pipelineName }));
-            var activeRuns = this.pipelineClient.QueryPipelineRunsByWorkspace(filter);
-            PipelineRun activityRun = activeRuns.Value.Value.LastOrDefault();
+            var activeRuns = this.pipelineClient.QueryPipelineRunsByWorkspace(filter).Value.Value;
+
+            if (parameterFilters != null && parameterFilters.Any())
+            {
+                activeRuns = activeRuns
+                    .Where(run => parameterFilters.All(parameterFilter =>
+                        run.Parameters.ContainsKey(parameterFilter.Key) &&
+                        run.Parameters[parameterFilter.Key] == parameterFilter.Value))
+                    .ToList();
+            }
+
+            PipelineRun activityRun = activeRuns.LastOrDefault();
 
             if (activityRun == null)
             {
-                throw new ExecutionWaitException($"No Run Id detected for { pipelineName }");
+                throw new ExecutionWaitException($"No Run Id detected for { pipelineName } with the given parameters");
             }
             else
             {
